9530235197c4d0a0639e4089b868e6d39477e3de
Haille <hmlperkins@hotmail.com>
2025-06-23 19:36:54 -0500

    mention intel lockfile and tabs for different platforms to install docs

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ extensions = [
     "sphinx_automodapi.automodapi",
     "sphinx_automodapi.smart_resolver",
     "sphinxcontrib.apidoc",
+    "sphinx_tabs.tabs"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
